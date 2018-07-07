9b451e2b8e285c665f2561d7cffcd468adb6f14a
Jonathan Jemson <jonathanjemson@gmail.com>
2018-07-06 18:05:28 -0700

    Refactor into Framework and binary, so that others can create custom ProjectUpKit scripts

--- Package.swift
+++ Package.swift
@@ -13,7 +13,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
+            name: "ProjectUpKit",
+            dependencies: ["Stencil"]
+        ),
+        .target(
             name: "projectup",
-            dependencies: ["Stencil"]),
+            dependencies: ["ProjectUpKit"]),
     ]
 )
