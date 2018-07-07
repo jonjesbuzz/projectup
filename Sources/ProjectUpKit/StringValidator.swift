a6a81d49b25feeda195a09247d0d615885c18d96
Jonathan Jemson <jonathanjemson@gmail.com>
2018-07-07 13:09:54 -0700

    Add StringValidator (not yet integrated)

---
+++ Sources/ProjectUpKit/StringValidator.swift
@@ -0,0 +1,23 @@
+//
+//  StringValidator.swift
+//  ProjectUpKit
+//
+//  Created by Jonathan Jemson on 7/7/18.
+//  Copyright © 2018 Jonathan Jemson. 
+//
+
+import Foundation
+
+extension String {
+    func isValidIdentifier() -> Bool {
+        let identifier = self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        if let firstChar = identifier.first {
+            let s = String(firstChar).trimmingCharacters(in: CharacterSet.decimalDigits)
+            if s.isEmpty { return false }
+        }
+        if identifier.contains(" ") {
+            return false
+        }
+        return !identifier.isEmpty
+    }
+}
