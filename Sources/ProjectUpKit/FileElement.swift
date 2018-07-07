dae969d91fb202506e2fcee0526ec7ef175f8f3b
Jonathan Jemson <jonathanjemson@gmail.com>
2018-07-07 11:12:28 -0700

    Convenience Sequence<FileElement> extension for scripts

--- Sources/ProjectUpKit/FileElement.swift
+++ Sources/ProjectUpKit/FileElement.swift
@@ -4,3 +4,13 @@ public protocol FileElement: CustomStringConvertible {
     var filename: String { get }
     func create(at url: URL) throws
 }
+
+// This enables you to call `create(at:)` on a FileElement sequence type.
+// Useful for scripting instead of creating a whole class structure.
+extension Sequence where Element == FileElement {
+    func create(at url: URL) throws {
+        for file in self {
+            try file.create(at: url)
+        }
+    }
+}
